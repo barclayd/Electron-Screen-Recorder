c1284fddd59a6873c4366a8e61ffe63c6d9cfedb
Daniel Barclay <barclaysd@me.com>
2020-03-06 18:50:48 +0000

    Select and show preview in <video /> for selected window

--- src/render.ts
+++ src/render.ts
@@ -20,6 +20,24 @@ const getVideoSources = async () => {
   videoOptions.popup();
 };
 
-const selectSource = (source: Electron.DesktopCapturerSource) => {};
+const selectSource = async (source: Electron.DesktopCapturerSource) => {
+  videoSelectButton.innerText = source.name;
+
+  const constraints: any = {
+    audio: false,
+    video: {
+      mandatory: {
+        chromeMediaSource: 'desktop',
+        chromeMediaSourceId: source.id,
+      },
+    },
+  };
+
+  // has to be defined separately before being passed as a reference to video.srcObject
+  const stream = await navigator.mediaDevices.getUserMedia(constraints);
+
+  video.srcObject = stream;
+  video.play();
+};
 
 videoSelectButton.onclick = getVideoSources;
