a77c8e974fc076dfd895f48760db98f8580d8864
Daniel Barclay <barclaysd@me.com>
2020-03-06 18:40:55 +0000

    Fix implementation of typescript within electron app. List screens available for recording

--- src/render.ts
+++ src/render.ts
@@ -1,4 +1,5 @@
-import { desktopCapturer } from 'electron';
+const { desktopCapturer, remote } = require('electron');
+const { Menu } = remote;
 
 const video = document.querySelector('video');
 const startBtn = document.getElementById('startBtn');
@@ -9,6 +10,16 @@ const getVideoSources = async () => {
   const inputSources = await desktopCapturer.getSources({
     types: ['window', 'screen'],
   });
+
+  const videoOptions = Menu.buildFromTemplate(
+    inputSources.map((source) => ({
+      label: source.name,
+      click: () => selectSource(source),
+    })),
+  );
+  videoOptions.popup();
 };
+
+const selectSource = (source: Electron.DesktopCapturerSource) => {};
 
 videoSelectButton.onclick = getVideoSources;
